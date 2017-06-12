a0a6b4099d1c6d520d929dd5ded85eb7aeec698c
Wesley Chalmers <wesleyjchalmers@gmail.com>
2017-06-12 18:30:33 -0400

    Add code

--- ender.py
+++ ender.py
@@ -1,5 +1,7 @@
-import random, math
-import pygame
+from __future__ import division
+
+import random, math, time
+import pygame, cwiid
 pygame.init()
 
 WIDTH, HEIGHT = 1000, 700
@@ -13,8 +15,8 @@ class Field():
         starPos = [[int(WIDTH*(1/3)-50), int(HEIGHT*(1/4)-50)],
                    [int(WIDTH*(1/3)-50), int(HEIGHT*(3/4)-50)],
                    [int(WIDTH*(2/3)-50), int(HEIGHT*(1/4)-50)],
-                   [int(WIDTH*(2/3)-50), int(HEIGHT*(3/4)-50)],
-                   [int(WIDTH*(1/2)-50), int(HEIGHT*(1/2)-50)]]
+                   [int(WIDTH*(2/3)-50), int(HEIGHT*(3/4)-50)]]
+                   #[int(WIDTH*(1/2)-50), int(HEIGHT*(1/2)-50)]]
         for pos in starPos:
             screen.blit(star, pos)
         pygame.draw.circle(screen, (255, 127, 0), (-200, int(HEIGHT/2)), 250)
@@ -25,25 +27,30 @@ class Player():
         self.x = x
         self.y = y
         self.heading = math.atan((WIDTH/2-x)/(HEIGHT/2-y))
-        if self.y > WIDTH/2:
+        if self.x > WIDTH/2:
             self.heading += math.pi
         self.color = color
+        self.v = 0
+        self.frozen = False
+        
     def generate(self):
-        pygame.draw.circle(screen, self.color, (self.x, self.y), 30)
+        pygame.draw.circle(screen, self.color, (int(self.x), int(self.y)), 30)
         #if self.y < HEIGHT/2:
         #    line_end = (40*math.sin(self.heading)+self.x,
         #                40*math.cos(self.heading)+self.y)
         #else:
         #    line_end = (-40*math.sin(self.heading)+self.x,
         #                -40*math.cos(self.heading)+self.y)
-        line_end = (40*math.sin(self.heading)+self.x,
-                    40*math.cos(self.heading)+self.y)
-        pygame.draw.line(screen, self.color, (self.x, self.y), line_end, 5)
+        line_end = (40*math.cos(self.heading)+int(self.x),
+                    40*math.sin(self.heading)+int(self.y))
+        pygame.draw.line(screen, self.color, (int(self.x), int(self.y)), line_end, 5)
     def turn(self, h):
         self.heading += h
     def move(self):
-        line_end = (20*math.sin(self.heading)+self.x,
-                    20*math.cos(self.heading)+self.y)
+        if self.frozen:
+            return
+        line_end = (self.v*math.cos(self.heading)+self.x,
+                    self.v*math.sin(self.heading)+self.y)
         #if self.y < HEIGHT/2:
         #    line_end = (20*math.sin(self.heading)+self.x,
         #                20*math.cos(self.heading)+self.y)
@@ -51,14 +58,18 @@ class Player():
         #    line_end = (-20*math.sin(self.heading)+self.x,
         #                -20*math.cos(self.heading)+self.y)
         self.x, self.y = line_end
-        self.x, self.y = int(self.x), int(self.y)
+        #self.x, self.y = int(self.x), int(self.y)
+        self.v *= 0.98 # Near-vacuum so little slowdown.
+
+    def shoot(self):
+        Flash(self.x, self.y, self.heading)
 
     def check_collision(self):
         collisionZones = [[int(WIDTH*(1/3)-80), int(HEIGHT*(1/4)-80)],
                           [int(WIDTH*(1/3)-80), int(HEIGHT*(3/4)-80)],
                           [int(WIDTH*(2/3)-80), int(HEIGHT*(1/4)-80)],
-                          [int(WIDTH*(2/3)-80), int(HEIGHT*(3/4)-80)],
-                          [int(WIDTH*(1/2)-80), int(HEIGHT*(1/2)-80)]]
+                          [int(WIDTH*(2/3)-80), int(HEIGHT*(3/4)-80)]]
+                         # [int(WIDTH*(1/2)-80), int(HEIGHT*(1/2)-80)]]
         while self.heading > 2*math.pi:
             self.heading -= 2*math.pi
         while self.heading < 0:
@@ -66,140 +77,133 @@ class Player():
         for zone in collisionZones:
             if zone[0] < self.x < zone[0]+160 and \
                zone[1] < self.y < zone[1]+160:
-                if self.y < zone[1]+30:
-                    if self.x < zone[0]+30 or self.x > zone[0]+130:
-                        print("Collision with top corner")
-                        self.heading -= math.pi
-                        if self.x < zone[0]+30:
-                            self.x, self.y = zone[0]-10, zone[1]-10
-                        else:
-                            self.x, self.y = zone[0]+170, zone[1]-10
-                    else:
-                        print("Collision with top side")
-                        print(self.heading)
-                        if self.heading > math.pi:
-                            self.heading -= math.pi/2
-                        else:
-                            self.heading += math.pi/2
-                        self.y = zone[1]-10
-                elif self.y > zone[1]+130:
-                    if self.x < zone[0]+30 or self.x > zone[0]+130:
-                        print("Collision with bottom corner")
-                        self.heading -= math.pi
-                        if self.x < zone[0]+30:
-                            self.x, self.y = zone[0]-10, zone[1]+170
-                        else:
-                            self.x, self.y = zone[0]+170, zone[1]+170
-                    else:
-                        print("Collision with bottom side")
-                        print(self.heading)
-                        if self.heading > math.pi:
-                            self.heading += math.pi/2
-                        else:
-                            self.heading -= math.pi/2
-                        self.y = zone[1]+170
+                self.v = 0
+                if self.y < zone[1]+30 and zone[0]+30 < self.x < zone[0]+130:
+                    self.heading = -self.heading
+                    self.y = zone[1]-10
+                elif self.y > zone[1]+130 and zone[0]+30 < self.x < zone[0]+130:
+                    self.heading = -self.heading
+                    self.y = zone[1]+170
                 else:
                     if self.x < zone[0]+80:
-                        print("Collision with left side")
-                        if self.heading > (3/2)*math.pi:
-                            self.heading += math.pi/2
-                        else:
-                            self.heading -= math.pi/2
+                        self.heading = -self.heading + math.pi
                         self.x = zone[0]-10
                     else:
-                        print("Collision with right side")
-                        print(self.heading)
-                        if self.heading > (3/2)*math.pi:
-                            self.heading += math.pi/2
-                        else:
-                            self.heading -= math.pi/2
+                        self.heading = -self.heading + math.pi
                         self.x = zone[0]+170
 
             if self.y < 10:
-                print("Collision with top")
-                if self.heading > math.pi:
-                    self.heading += math.pi/2
-                else:
-                    self.heading -= math.pi/2
-                self.y = 15
+                self.heading = -self.heading
+                self.y = 10
             elif self.y > HEIGHT-10:
-                print("Collision with bottom")
-                if self.heading > math.pi:
-                    self.heading -= math.pi/2
-                else:
-                    self.heading += math.pi/2
-                self.y = HEIGHT-15
+                self.heading = -self.heading
+                self.y = HEIGHT-10
             if self.x < 10:
-                print("Collision with left")
-                if self.heading > (3/2)*math.pi:
-                    self.heading += math.pi/2
-                else:
-                    self.heading -= math.pi/2
+                self.heading = -self.heading + math.pi
                 self.x = 15
             elif self.x > WIDTH-10:
-                print("Collision with right")
-                if self.heading > (3/2)*math.pi:
-                    self.heading += math.pi/2
-                else:
-                    self.heading -= math.pi/2
+                self.heading = -self.heading + math.pi
                 self.x = WIDTH-10
 
+    def freeze(self):
+        self.frozen = True
+        self.v = 0
 
-f = Field()
-f.generate()
-
-players = [Player(60, 60, (0, 255, 127)),
-           Player(60, HEIGHT-60, (0, 255, 127)),
-           Player(WIDTH-60, 60, (255, 127, 0)),
-           Player(WIDTH-60, HEIGHT-60, (255, 127, 0))]
-
-for p in players:
-    p.generate()
-
-q = False
-while not q:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            q = True
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
-                q = True
-            elif event.key == ord("a"):
-                players[0].turn(0.5)
-            elif event.key == ord("s"):
-                players[0].turn(-0.5)
-            elif event.key == ord("w"):
-                players[0].move()
-            elif event.key == ord("f"):
-                players[1].turn(0.5)
-            elif event.key == ord("g"):
-                players[1].turn(-0.5)
-            elif event.key == ord("t"):
-                players[1].move()
-            elif event.key == ord("h"):
-                players[2].turn(0.5)
-            elif event.key == ord("j"):
-                players[2].turn(-0.5)
-            elif event.key == ord("u"):
-                players[2].move()
-            elif event.key == ord("k"):
-                players[3].turn(0.5)
-            elif event.key == ord("l"):
-                players[3].turn(-0.5)
-            elif event.key == ord("o"):
-                players[3].move()
-
-    for player in players:
-        player.check_collision()
-
-    screen.fill((0, 0, 0))
-
-    f.generate()
-    for player in players:
-        player.generate()
-        
-    pygame.display.flip()
+def reset():
+    global flashes, players, f
+    flashes = []
     
-            
+    players = [Player(60, 60, (0, 255, 127)),
+               Player(60, HEIGHT-60, (0, 255, 127)),
+               Player(WIDTH-60, 60, (255, 127, 0)),
+               Player(WIDTH-60, HEIGHT-60, (255, 127, 0))]
+    f = Field(); f.generate()
+    for p in players:
+        p.generate()
+    
+reset()
+    
+class Flash():
+    def __init__(self, x, y, h):
+        self.x = x
+        self.y = y
+        self.heading = h
+        flashes.append(self)
 
+    def generate(self):
+        self.x += 20*math.cos(self.heading)
+        self.y += 20*math.sin(self.heading)
+        pygame.draw.circle(screen, (255, 255, 255), (int(self.x), int(self.y)), 5)
+        if self.x < 0 or self.x > WIDTH or self.y < 0 or self.y > HEIGHT:
+            flashes.remove(self)
+        for p in players:
+            if math.sqrt((self.x-p.x)**2 + (self.y-p.y)**2) < 10:
+                p.freeze()
+
+
+wms = []
+while len(wms) < 4:
+    try:
+        print("Attempting connection number "+str(len(wms)))
+        wms.append(cwiid.Wiimote())
+        wms[len(wms)-1].rpt_mode = cwiid.RPT_BTN
+    except Exception as e:
+        print("Fail! "+str(e))
+    else:
+        print("Success!")
+
+c = pygame.time.Clock()
+
+font = pygame.font.SysFont("Ubuntu Mono", 30)
+
+def runGame():
+    q = False
+    global winner
+    while not q:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit()
+            elif event.type == pygame.KEYDOWN:
+                if (event.key == pygame.K_ESCAPE): exit()
+        for i, p in enumerate(players):
+            buttons = wms[i].state["buttons"]
+            #print(buttons)
+            if buttons & cwiid.BTN_LEFT:
+                p.turn(-0.02)
+            if buttons & cwiid.BTN_RIGHT:
+                p.turn(0.02)
+            if buttons & cwiid.BTN_UP:
+                p.v += 0.2
+            if buttons & cwiid.BTN_B:
+                p.shoot()
+            p.move()
+            p.check_collision()
+
+        screen.fill((0, 0, 0))
+
+        f.generate()
+        for player in players:
+            player.generate()
+        for flash in flashes:
+            flash.generate()
+
+        if players[0].frozen and players[1].frozen or players[2].frozen and players[3].frozen:
+            q = True
+            if players[0].frozen:
+                winner = "Orange"
+            else:
+                winner = "Green"
+        
+        pygame.display.flip()
+        c.tick(30)
+            
+while True:
+    runGame()
+    screen.blit(font.render(winner+" Wins!", True, (255, 127, 0) if winner == "Orange" else (0, 255, 127)), (WIDTH/2-font.size(winner+" Wins!")[0]/2, HEIGHT/2-font.size(winner+" Wins!")[1]/2))
+    pygame.display.flip()
+    pygame.time.wait(10000)
+    q = False
+    reset()
+
+        
 pygame.quit()
